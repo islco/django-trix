5d9fe4158f070a88ddcc744220bc813fda1d46b9
Jeremy Carbaugh <jeremy@isl.co>
2015-10-20 15:30:16 -0400

    make html params a dict and shorten line for pep8

--- trix/widgets.py
+++ trix/widgets.py
@@ -11,9 +11,14 @@ class TrixEditor(forms.Textarea):
             attrs = {}
         attrs.update({'style': 'visibility: hidden; position: absolute;'})
 
-        id_attr = attrs.get('id') or '{}_id'.format(name)
+        params = {
+            'input': attrs.get('id') or '{}_id'.format(name),
+            'class': 'trix-content',
+        }
+        param_str = ' '.join('{}="{}"'.format(k, v) for k, v in params.items())
+
         html = super(TrixEditor, self).render(name, value, attrs)
-        html += """<p><trix-editor input="{}" class="trix-content"></trix-editor></p>""".format(id_attr)
+        html += '<p><trix-editor {}></trix-editor></p>'.format(param_str)
         return html
 
     class Media:
