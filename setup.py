bf15fd1219dfb0951713bc93385c0d4f116ba668
Jeremy Carbaugh <jeremy@isl.co>
2015-11-22 23:20:13 -0500

    bump version to make sure html output is safe

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ long_description = open('README.rst').read()
 
 setup(
     name="django-trix",
-    version='0.2.1',
+    version='0.2.2',
     packages=["trix"],
     include_package_data=True,
     description="Trix rich text editor widget for Django",
