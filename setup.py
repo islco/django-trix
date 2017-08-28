c5da56e5398e3ae8038ba280736e56b527f06e7d
Jeremy Carbaugh <jeremy@jcarbaugh.com>
2017-08-28 12:53:24 -0400

    add Python 3 language classifiers (#8)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ long_description = open('README.rst').read()
 
 setup(
     name="django-trix",
-    version='0.3.0',
+    version='0.3.1',
     packages=["trix"],
     include_package_data=True,
     description="Trix rich text editor widget for Django",
@@ -21,6 +21,9 @@ setup(
         "Intended Audience :: Developers",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
 )
